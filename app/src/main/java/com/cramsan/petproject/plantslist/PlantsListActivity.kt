5f4a3ed6b70e6fabd6f82c3f356bd419694c9734
Cesar Ramirez <contact+gh@cramsan.com>
2019-06-02 20:09:58 -0700

    Adding RecyclerView in PlantsList

--- app/src/main/java/com/cramsan/petproject/plantslist/PlantsListActivity.kt
+++ app/src/main/java/com/cramsan/petproject/plantslist/PlantsListActivity.kt
@@ -3,11 +3,11 @@ package com.cramsan.petproject.plantslist
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity;
 import com.cramsan.petproject.R
+import com.cramsan.petproject.plantslist.dummy.DummyContent
 
 import kotlinx.android.synthetic.main.activity_plants_list.*
 
-class PlantsListActivity : AppCompatActivity() {
-
+class PlantsListActivity : AppCompatActivity(), PlantsListFragment.OnListFragmentInteractionListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plants_list)
@@ -16,4 +16,7 @@ class PlantsListActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
--- app/src/main/java/com/cramsan/petproject/plantslist/PlantsListFragment.kt
+++ app/src/main/java/com/cramsan/petproject/plantslist/PlantsListFragment.kt
@@ -1,21 +1,100 @@
 package com.cramsan.petproject.plantslist
 
-import androidx.fragment.app.Fragment
+import android.content.Context
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.cramsan.petproject.R
 
+import com.cramsan.petproject.plantslist.dummy.DummyContent
+import com.cramsan.petproject.plantslist.dummy.DummyContent.DummyItem
+
 /**
- * A placeholder fragment containing a simple view.
+ * A fragment representing a list of Items.
+ * Activities containing this fragment MUST implement the
+ * [PlantsListFragment.OnListFragmentInteractionListener] interface.
  */
 class PlantsListFragment : Fragment() {
+
+    // TODO: Customize parameters
+    private var columnCount = 1
+
+    private var listener: OnListFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_plants_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_plants_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = PlantsRecyclerViewAdapter(DummyContent.ITEMS, listener)
+            }
+        }
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnListFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson
+     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onListFragmentInteraction(item: DummyItem?)
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            PlantsListFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
     }
 }
