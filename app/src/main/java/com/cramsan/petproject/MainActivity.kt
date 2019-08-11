7d3ee52df125bb487a97f85f378154ceb1428a9e
Cesar Ramirez <contact+gh@cramsan.com>
2019-08-10 17:09:12 -0700

    Improved Main Activity layout

--- app/src/main/java/com/cramsan/petproject/MainActivity.kt
+++ app/src/main/java/com/cramsan/petproject/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
@@ -26,6 +27,10 @@ import kotlinx.android.synthetic.main.activity_main.nav_view
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
 import org.kodein.di.erased.instance
+import androidx.core.view.MenuItemCompat
+import android.view.View.OnFocusChangeListener
+
+
 
 class MainActivity : AppCompatActivity(),
     NavigationView.OnNavigationItemSelectedListener,
@@ -66,6 +71,14 @@ class MainActivity : AppCompatActivity(),
             } else {
                 selectedMenuItem = nav_view.menu.findItem(tabToLoad)
                 selectedTabId = tabToLoad
+                when (selectedMenuItem.itemId) {
+                    R.id.nav_home -> {
+                        supportActionBar?.setTitle(R.string.title_fragment_plants_cats)
+                    }
+                    R.id.nav_gallery -> {
+                        supportActionBar?.setTitle(R.string.title_fragment_plants_dogs)
+                    }
+                }
             }
         } else {
             selectedMenuItem = nav_view.menu.findItem(tabToLoad)
@@ -108,8 +121,7 @@ class MainActivity : AppCompatActivity(),
             setSearchableInfo(searchManager.getSearchableInfo(componentName))
             setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String): Boolean {
-                    queryTextListener?.onQueryTextSubmit(query)
-                    return true
+                    return false
                 }
 
                 override fun onQueryTextChange(newText: String): Boolean {
@@ -138,10 +150,6 @@ class MainActivity : AppCompatActivity(),
 
         transaction.replace(R.id.main_container, newFragment)
         transaction.commit()
-        when (animalType) {
-            AnimalType.CAT -> supportActionBar?.setTitle(R.string.title_fragment_plants_cats)
-            AnimalType.DOG -> supportActionBar?.setTitle(R.string.title_fragment_plants_dogs)
-        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -158,22 +166,16 @@ class MainActivity : AppCompatActivity(),
         // Handle navigation view item clicks here.
         when (item.itemId) {
             R.id.nav_home -> {
+                supportActionBar?.setTitle(R.string.title_fragment_plants_cats)
                 setFragmentForAnimalType(AnimalType.CAT)
             }
             R.id.nav_gallery -> {
+                supportActionBar?.setTitle(R.string.title_fragment_plants_dogs)
                 setFragmentForAnimalType(AnimalType.DOG)
-            }
-            R.id.nav_share -> {
-            }
-            R.id.nav_send -> {
             }
         }
         selectedTabId = item.itemId
         return true
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
     }
 
     override fun onListFragmentInteraction(plantId: Int, animalType: AnimalType) {
