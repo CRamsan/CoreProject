535bef0c74d8b68b4ed19733bce3c1c62daacb86
Cesar Ramirez <contact+gh@cramsan.com>
2019-08-12 20:47:25 -0700

    Adding About activity with toolbar

--- app/src/main/java/com/cramsan/petproject/about/AboutActivity.kt
+++ app/src/main/java/com/cramsan/petproject/about/AboutActivity.kt
@@ -3,11 +3,16 @@ package com.cramsan.petproject.about
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cramsan.petproject.R
+import kotlinx.android.synthetic.main.activity_about.toolbar_2
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        setSupportActionBar(toolbar_2)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
