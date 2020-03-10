2e5bafb71c24c5428a0051ef0c5ef0538ffb5038
Cesar Ramirez <contact+gh@cramsan.com>
2020-03-09 21:24:56 -0700

    Fixing the API Keys

--- auraxiscontrolcenter/src/main/java/com/cesarandres/ps2link/ApplicationPS2Link.kt
+++ auraxiscontrolcenter/src/main/java/com/cesarandres/ps2link/ApplicationPS2Link.kt
@@ -117,7 +117,7 @@ class ApplicationPS2Link : Application(), KodeinAware {
     override fun onCreate() {
         super.onCreate()
         eventLogger.log(Severity.INFO, classTag(), "onCreate called")
-        AppCenter.start(this, "1206f21f-1b20-483f-9385-9b8cbc0e504d")
+        AppCenter.start(this, "2cbdd11d-e4ef-4626-b09f-2a7deb82664a")
         crashHandler.initialize()
         metrics.initialize()
 
