a438fd8b77ba66792f61208bf2f16e0624bb8f60
Cesar Ramirez <contact@cramsan.com>
2021-05-09 23:31:00 -0700

    Centralizing the repository management

--- auraxiscontrolcenter/appcore/src/commonTest/kotlin/com/cramsan/ps2link/appcore/DBGServiceClientImplTest.kt
+++ auraxiscontrolcenter/appcore/src/commonTest/kotlin/com/cramsan/ps2link/appcore/DBGServiceClientImplTest.kt
@@ -6,17 +6,21 @@ import com.cramsan.ps2link.appcore.census.DBGServiceClient
 import com.cramsan.ps2link.appcore.census.DBGServiceClientImpl
 import com.cramsan.ps2link.appcore.network.HttpClient
 import io.mockk.impl.annotations.MockK
+import kotlinx.datetime.Clock
 
 class DBGServiceClientImplTest : TestBase() {
 
     @MockK
     lateinit var http: HttpClient
 
+    @MockK
+    lateinit var clock: Clock
+
     lateinit var dbgServiceClient: DBGServiceClient
 
     override fun setupTest() {
         super.setupTest()
-        dbgServiceClient = DBGServiceClientImpl(DBGCensus(), http)
+        dbgServiceClient = DBGServiceClientImpl(DBGCensus(), http, clock)
     }
 
     fun `test searching for a profile by name`() = runBlockingTest {
