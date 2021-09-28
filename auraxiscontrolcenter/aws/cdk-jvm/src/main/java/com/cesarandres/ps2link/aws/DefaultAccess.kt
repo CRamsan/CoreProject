c6d29d9a32a2c2482bfdec018bded39799f0a262
Cesar Ramirez <contact+gh@cramsan.com>
2021-09-27 18:46:03 -0700

    [PS2Link] Removed Secret from ReadOnly user in CDK

--- auraxiscontrolcenter/aws/cdk-jvm/src/main/java/com/cesarandres/ps2link/aws/DefaultAccess.kt
+++ auraxiscontrolcenter/aws/cdk-jvm/src/main/java/com/cesarandres/ps2link/aws/DefaultAccess.kt
@@ -6,21 +6,11 @@ import software.amazon.awscdk.services.iam.CfnAccessKey
 import software.amazon.awscdk.services.iam.ManagedPolicy
 import software.amazon.awscdk.services.iam.User
 import software.amazon.awscdk.services.iam.UserProps
-import software.amazon.awscdk.services.secretsmanager.Secret
 
 class DefaultAccess(scope: software.constructs.Construct, id: String) : Construct(scope, id) {
 
     init {
-        val secret = Secret(this, "readOnlyUser")
-        val readOnlyUser = User(
-            this,
-            "ReadOnly",
-            UserProps.builder()
-                .password(secret.secretValue)
-                .passwordResetRequired(true)
-                .build()
-        )
-        secret.grantRead(readOnlyUser)
+        val readOnlyUser = User(this, "ReadOnly", null)
         readOnlyUser.addManagedPolicy(
             ManagedPolicy.fromAwsManagedPolicyName("CloudWatchReadOnlyAccess")
         )
