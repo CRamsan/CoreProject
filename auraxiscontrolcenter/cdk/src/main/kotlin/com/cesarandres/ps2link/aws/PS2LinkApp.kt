6128ce34819db176cc8407099a25367d3bcf5bdd
Cesar Ramirez <contact@cramsan.com>
2022-01-25 23:54:45 -0800

    [PS2Link] Adjusting underflow alarm

--- auraxiscontrolcenter/cdk/src/main/kotlin/com/cesarandres/ps2link/aws/PS2LinkApp.kt
+++ auraxiscontrolcenter/cdk/src/main/kotlin/com/cesarandres/ps2link/aws/PS2LinkApp.kt
@@ -30,28 +30,24 @@ object PS2LinkApp {
     @JvmStatic
     fun main(args: Array<String>) {
         val app = App()
-        val props = StackProps.builder() // If you don't specify 'env', this stack will be environment-agnostic.
-            // Account/Region-dependent features and context lookups will not work,
-            // but a single synthesized template can be deployed anywhere.
-            // Uncomment the next block to specialize this stack for the AWS Account
-            // and Region that are implied by the current CLI configuration.
-            /*
-            .env(Environment.builder()
-                    .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                    .region(System.getenv("CDK_DEFAULT_REGION"))
-                    .build())
-            */
-            // Uncomment the next block if you know exactly what Account and Region you
-            // want to deploy the stack to.
-            /*
-            .env(Environment.builder()
-                    .account("123456789012")
-                    .region("us-east-1")
-                    .build())
-            */
-            // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-            .build()
+        val props = buildProps()
+        buildMetricsStack(app, props)
+        buildRemoteConfigStack(app, props)
+    }
 
+    private fun buildRemoteConfigStack(app: App, props: StackProps) {
+        val payload = initializePayload(remoteConfigJson, "ps2Link", remoteConfigPayload)
+        RemoteConfigStack(
+            app,
+            "PS2LinkRemoteConfig",
+            payload,
+            props,
+        )
+        app.synth()
+    }
+
+    @Suppress("LongMethod")
+    private fun buildMetricsStack(app: App, props: StackProps) {
         MetricsStack(
             app,
             "PS2LinkApp",
@@ -80,7 +76,7 @@ object PS2LinkApp {
                     .metric(launchMetric)
                     .comparisonOperator(ComparisonOperator.LESS_THAN_THRESHOLD)
                     .threshold(4.0)
-                    .evaluationPeriods(2)
+                    .evaluationPeriods(3)
                     .datapointsToAlarm(2)
                     .build()
             ).apply {
@@ -114,14 +110,28 @@ object PS2LinkApp {
                 addAlarmAction(SnsAction(emailTopic))
             }
         }
-
-        val payload = initializePayload(remoteConfigJson, "ps2Link", remoteConfigPayload)
-        RemoteConfigStack(
-            app,
-            "PS2LinkRemoteConfig",
-            payload,
-            props,
-        )
-        app.synth()
     }
+
+    private fun buildProps() = StackProps.builder()
+        // If you don't specify 'env', this stack will be environment-agnostic.
+        // Account/Region-dependent features and context lookups will not work,
+        // but a single synthesized template can be deployed anywhere.
+        // Uncomment the next block to specialize this stack for the AWS Account
+        // and Region that are implied by the current CLI configuration.
+        /*
+        .env(Environment.builder()
+                .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                .region(System.getenv("CDK_DEFAULT_REGION"))
+                .build())
+        */
+        // Uncomment the next block if you know exactly what Account and Region you
+        // want to deploy the stack to.
+        /*
+        .env(Environment.builder()
+                .account("123456789012")
+                .region("us-east-1")
+                .build())
+        */
+        // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+        .build()
 }
