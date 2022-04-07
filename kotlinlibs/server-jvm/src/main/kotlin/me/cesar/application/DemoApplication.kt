207d309e16bec9d613683d9e1e60c7a1bc1147db
Cesar Ramirez <contact@cramsan.com>
2022-04-06 23:41:39 -0700

    [Kotlinlibs] Added react front end

--- kotlinlibs/server-jvm/src/main/kotlin/me/cesar/application/DemoApplication.kt
+++ kotlinlibs/server-jvm/src/main/kotlin/me/cesar/application/DemoApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableConfigurationProperties(BlogProperties::class)
 @SpringBootApplication
 @EnableScheduling
 /**
