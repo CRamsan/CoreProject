ecee0c5a276588679bba545e6d9fbf3c5d0fed7d
Cesar Ramirez <contact+gh@cramsan.com>
2023-05-28 15:39:49 -0700

    [PS2Link] Removed extra dependencies

--- auraxiscontrolcenter/db-models/build.gradle.kts
+++ auraxiscontrolcenter/db-models/build.gradle.kts
@@ -25,20 +25,6 @@ kotlin {
         }
         val commonMain by getting {
             dependencies {
-                implementation(project(":framework:assert"))
-                implementation(project(":framework:crashhandler"))
-                implementation(project(":framework:core"))
-                implementation(project(":framework:halt"))
-                implementation(project(":framework:interfacelib"))
-                implementation(project(":framework:interfacelib-test"))
-                implementation(project(":framework:logging"))
-                implementation(project(":framework:metrics"))
-                implementation(project(":framework:userevents"))
-                implementation(project(":framework:preferences"))
-                implementation(project(":framework:remoteconfig"))
-                implementation(project(":framework:thread"))
-                implementation(project(":framework:test"))
-                implementation(project(":framework:utils"))
                 implementation(Square.sqlDelight.extensions.coroutines)
                 implementation(KotlinX.datetime)
             }
