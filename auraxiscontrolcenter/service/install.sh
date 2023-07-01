47c9f16b1cd60c1c38055502b4d80995b3543363
CI <CI@derpcompany.com>
2023-07-01 07:26:19 +0000

    [PS2Link] Fixed install file

--- auraxiscontrolcenter/service/install.sh
+++ auraxiscontrolcenter/service/install.sh
@@ -11,25 +11,50 @@ SYSTEMD_FOLDER="$HOME/.config/systemd/user"
 
 echo "Building Server Executable"
 sleep 2
-./gradlew buildFatJar
+../../gradlew :auraxiscontrolcenter:service:buildFatJar
+
+if [ $? -ne 0 ]; then
+    echo "Build was unsuccesful"
+    exit $?
+fi
 
 echo "Creating $INSTALLATION_FOLDER"
 mkdir -p $INSTALLATION_FOLDER
+if [ $? -ne 0 ]; then
+    echo "Could not create installation folder $INSTALLATION_FOLDER"
+    exit $?
+fi
 
 touch $INSTALLATION_FOLDER/$PROPERTIES_FILE
+if [ $? -ne 0 ]; then
+    echo "Could not create configuration file $INSTALLATION_FOLDER/$PROPERTIES_FILE"
+    exit $?
+fi
 
 export XDG_RUNTIME_DIR=/run/user/$(id -u $USER)
 export DBUS_SESSION_BUS_ADDRESS=/run/user/$(id -u $USER)/bus
 
 echo "Stopping server..."
 systemctl --user stop $SYSTEMD_FILE
+if [ $? -ne 0 ]; then
+    echo "Could not stop service $SYSTEMD_FILE"
+    exit $?
+fi
 
 echo "Copying file $ARTIFACT_FOLDER/$ARTIFACT_FILE to $INSTALLATION_FOLDER/$ARTIFACT_FILE"
 cp $ARTIFACT_FOLDER/$ARTIFACT_FILE $INSTALLATION_FOLDER/$ARTIFACT_FILE
+if [ $? -ne 0 ]; then
+    echo "Could not copy artifact"
+    exit $?
+fi
 
 echo "Installing systemd unit $SYSTEMD_FILE in $SYSTEMD_FOLDER/$SYSTEMD_FILE"
 mkdir -p $SYSTEMD_FOLDER
 cp -f $SYSTEMD_FILE $SYSTEMD_FOLDER/$SYSTEMD_FILE
+if [ $? -ne 0 ]; then
+    echo "Could not install service $SYSTEMD_FILE"
+    exit $?
+fi
 
 echo "Starting server"
 systemctl --user daemon-reload
